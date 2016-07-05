155daf8375e7aea56747eee8ffe006c5e73ebe29
jimmyMaci <tyler.durden69@gmx.de>
2016-07-05 18:18:49 +0200

    Update check.js

--- src/check.js
+++ src/check.js
@@ -2,9 +2,14 @@ var Check = function() {
 
 	var self = this;
 	self.isUndefined = isUndefined;
+	self.isString = isString;
 	
 	function isUndefined(object) {
 	return typeof object !== 'undefined';
 	}
+	
+	function isString(value) {
+	  	return (typeof value === 'string' || value instanceof String);
+	  }
 	
 };
